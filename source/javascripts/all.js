d98401157444a27e6562189f0c2cba9f968e8a68
aopuce <a.o.puce@gmail.com>
2015-11-19 15:42:17 +0100

    Aggiunto griglia responsive per la maggior parte degli elementi del layout, aggiunto jquery, masonry.js e swier.js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 //= require_tree .
+$(document).ready(function() {
+  $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+  var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      loop: true,
+      autoplay: 3500,
+      autoplayDisableOnInteraction: false,
+      effect: 'fade'
+    })
+});
